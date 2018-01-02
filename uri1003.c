25b77a68a891f478d1a19316902e6073f28e1a5b
alexandregust <gustavo.alexandre3@gmail.com>
2018-01-01 22:32:58 -0200

    1003

---
+++ uri1003.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "math.h"
+
+int main (){
+int A;
+int B;
+int SOMA;
+scanf("%d",&A);
+scanf("%d",&B);
+
+SOMA = A + B;
+
+printf("SOMA = %d\n",SOMA);
+
+
+
+
+return 0;
+
+
+
+}
