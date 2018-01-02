55f59160224070eb2ab7465e17481bea575bd15a
alexandregust <gustavo.alexandre3@gmail.com>
2018-01-01 22:17:07 -0200

    1002

---
+++ uri1002.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "math.h"
+
+int main (){
+
+double area;
+double raio;
+double n = 3.14159;
+
+scanf("%lf",&raio);
+
+
+area = n * pow(raio,2);
+
+printf("A=%.4lf\n",area);
+
+
+
+return 0;
+
+
+
+}
