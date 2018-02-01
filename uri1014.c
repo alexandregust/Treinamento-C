0f7f5e1ecb71db331bcf9ba22248419e07e20653
alexandregust <gustavo.alexandre3@gmail.com>
2018-02-01 01:02:20 -0200

    1014

---
+++ uri1014.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+int main (){
+
+
+
+int dist;
+float cons;
+float rend;
+
+scanf("%d\n",&dist);
+scanf("%f",&cons);
+
+
+rend = dist / cons;
+
+
+
+printf("%.3f km/l\n",rend);
+
+
+
+return 0;
+
+
+
+}
