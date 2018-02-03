5db15c68587f827ea1bbab468689eff15e1f2f13
alexandregust <gustavo.alexandre3@gmail.com>
2018-02-02 22:31:41 -0200

    1016

---
+++ uri1016.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+
+int temp , dist ;
+
+scanf("%d",&dist );
+
+
+temp = 2 * dist;
+
+printf("%d minutos\n",temp);
+
+return 0;
+
+}
